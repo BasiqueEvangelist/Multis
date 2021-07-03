310df8398e7f0f38d567d3954db62988b8ddf659
TheOneTrueZandra <aaron.mich.mills@gmail.com>
2021-07-03 07:29:53 -0600

    Added some basic stff

---
+++ common/src/main/java/net/blancworks/multis/objects/item/MultisItem.java
@@ -0,0 +1,11 @@
+package net.blancworks.multis.objects.item;
+
+/**
+ * A Multis Item is basically a wrapper for a Minecraft item that redirects calls to a script.
+ * They can be dynamically created or destroyed, as with most things in Multis.
+ */
+public class MultisItem {
+    
+    
+    
+}
---
+++ common/src/main/java/net/blancworks/multis/objects/item/MultisItemManager.java
@@ -0,0 +1,13 @@
+package net.blancworks.multis.objects.item;
+
+import net.blancworks.api.assets.BWAssetRegistery;
+
+public class MultisItemManager {
+    
+    public static BWAssetRegistery<MultisItem> itemRegistry = new BWAssetRegistery<>();
+    
+    
+    public static void loadItem(){
+        
+    }
+}
